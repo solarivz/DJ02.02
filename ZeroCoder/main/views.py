684d0347d65ce8800e51aa1fdfc477394a21dec0
Ivan Zyulin <solar.ivz@gmail.com>
2024-11-25 12:50:51 +1200

    upd main/ urls view

--- ZeroCoder/main/views.py
+++ ZeroCoder/main/views.py
@@ -1,4 +1,6 @@
+from django.shortcuts import render
 from django.http import HttpResponse
-
 def index(request):
-    return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+    return render(request, 'main/index.html')
+def new(request):
+    return HttpResponse("<h1>Это вторая страница моего проекта на django</h1>")
