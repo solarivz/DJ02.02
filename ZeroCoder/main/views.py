73874f917b653e5c01c061ba46da16b117b5b956
Ivan Zyulin <solar.ivz@gmail.com>
2024-11-25 12:59:15 +1200

    upd code
    main add files nav.html
    upd index, new, layout

--- ZeroCoder/main/views.py
+++ ZeroCoder/main/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'main/index.html')
 def new(request):
-    return HttpResponse("<h1>Это вторая страница моего проекта на django</h1>")
+    return render(request, 'main/new.html')
